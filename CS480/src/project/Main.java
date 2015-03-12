f340071c8c68b5323eae6a1245884e6e2a370c08
Vicken52 <krikorian.vicken@gmail.com>
2015-03-12 13:12:07 -0700

    Creating Files

---
+++ CS480/src/project/Main.java
@@ -0,0 +1,20 @@
+/* Have a menu-driven loop that allows the user to "send" or "receive"
+ * 		files. "Send" will simply prompt for an input filename and 
+ * 		process the file as specified in DigitalSignature.java. "Receive"
+ * 		will also prompt for a filename (should be a .signed file which
+ * 		had previously been output from a "Send") and process it as
+ * 		specified in DigitalSignature.java, outputting whether or not
+ * 		the file has been tampered with.
+ * */
+
+
+package project;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
